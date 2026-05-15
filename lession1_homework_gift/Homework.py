493544bf1bc8a216d59382d30a850f15e2b872d0
Makofane <F8877958@fnb.co.za>
2026-05-15 20:19:39 +0200

    Chnages to the homework

--- lession1_homework_gift/Homework.py
+++ lession1_homework_gift/Homework.py
@@ -1,7 +1,7 @@
 get_user1_name = input('Enter user1 name: ') #Enter and store user1 name
 get_user1_age = input('Enter user1 age: ') #Enter and store user1 age
 user1_age = int(get_user1_age) #Convert user1 age string to int
-get_user1_height = input('Enter user1 height: ') #Enter and store user1 age
+get_user1_height = input('Enter user1 height in inches: ') #Enter and store user1 age
 user1_height = float(get_user1_height) #Enter and store user1 height
 user1_height_in_cm = user1_height * 2.54
 
@@ -11,7 +11,7 @@ print()
 get_user2_name = input('Enter user2 name: ') #Enter and store user2 name
 get_user2_age = input('Enter user2 age: ') #Enter and store user2 age
 user2_age = int(get_user2_age) #Convert user2 age string to int
-get_user2_height = input('Enter user2 height: ') #Enter and store user2 age
+get_user2_height = input('Enter user2 height in inches: ') #Enter and store user2 age
 user2_height = float(get_user2_height) #Enter and store user2 height
 user2_height_in_cm = user2_height * 2.54
 
