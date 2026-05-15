566993bb435a0a4eb6aae68711b2d2d95e22e78c
Makofane <F8877958@fnb.co.za>
2026-05-15 19:38:24 +0200

    Lesson 1 commit

---
+++ lesson1/02_variables_and_data_types.py
@@ -0,0 +1,8 @@
+# string, int, float, boolean
+my_string = "Mary"
+
+print(my_string)
+
+print(type(my_string))
+
+print()  # empty line
---
+++ lesson1/04_get_user_input.py
@@ -0,0 +1,14 @@
+# name = input('Enter name')
+#
+# print(name)
+# print(type(name))
+
+age = input('Enter age')
+print(age)
+print(type(age))
+print()
+
+age = int(age)
+
+print(age)
+print(type(age))
